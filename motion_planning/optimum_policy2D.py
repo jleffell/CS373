0d15cedf6701fdbc20b136ccd5685cb5c90478f0
Joshua Leffell <jleffell@gmail.com>
2016-09-23 02:50:38 -0700

    Program works. Just need to add error checking in case car fails to find goal

--- motion_planning/optimum_policy2D.py
+++ motion_planning/optimum_policy2D.py
@@ -61,12 +61,72 @@ def optimum_policy2D(grid,init,goal,cost):
 
     max_val = 999
 
-    # index into value[theta][x][y]
-    value = [[[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
-             [[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
-             [[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
-             [[max_val for row in range(len(grid[0]))] for col in range (len(grid))]]
+    # index into value[theta][x][y]]
+    value   = [[[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[max_val for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[max_val for row in range(len(grid[0]))] for col in range (len(grid))]]
+    
+    actions = [[[-1 for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[-1 for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[-1 for row in range(len(grid[0]))] for col in range (len(grid))],
+               [[-1 for row in range(len(grid[0]))] for col in range (len(grid))]]
+    
+    policy  = [[' ' for row in range(len(grid[0]))] for col in range (len(grid))]
 
-#    return policy2D
+    change  = True
+
+    while change:
+        
+        change = False
+    
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                for theta in range(len(forward)):
+                    if goal[0] == x and goal[1] == y:
+                            if value[theta][x][y] > 0:
+                                value[theta][x][y] = 0
+                                change = True
+
+                    elif grid[x][y]==0:
+                        
+                        for a in range(len(action)):
+                            theta2 = (theta + action[a])%len(forward)
+                            x2     = x + forward[theta2][0]
+                            y2     = y + forward[theta2][1]
+
+                            if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
+
+                                v2 = value[theta2][x2][y2] + cost[a]
+                                
+                                if v2 < value[theta][x][y]: 
+                                    change = True
+                                    value[theta][x][y]  = v2
+                                    actions[theta][x][y] = a
+
+    #
+    # Follow Optimal Trajectory to Goal
+    # Todo: add check for failure!
+
+    x     = init[0]
+    y     = init[1]
+    theta = init[2]
+
+    driving = True
+    
+    while driving:
+
+        policy[x][y] = action_name[actions[theta][x][y]]
+
+        theta = (theta + action[actions[theta][x][y]])%len(forward)
+        x     = x + forward[theta][0]
+        y     = y + forward[theta][1]
+        
+        if x == goal[0] and y == goal[1]:
+            policy[x][y] = '*'
+            driving = False
+            
+    for i in range(len(grid)):
+        print policy[i]
 
 optimum_policy2D(grid,init,goal,cost)
